d3d3129af0be080f504990af42587849847b3695
Teemu Kuutti <teemu@identio.fi>
2021-02-04 15:17:36 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Hello morsenger
+        </h1>
+        <p>Like messenger but with morse code</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
